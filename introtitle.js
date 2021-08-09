03a6370beeaf41a405662b65cd26440c4c079990
KimHeroin <yebboona@snu.ac.kr>
2021-08-10 03:40:53 +0900

    c

---
+++ introtitle.js
@@ -0,0 +1,27 @@
+const letters = "AGENT";
+let textsize=90;
+
+function setup() {
+  var canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0, 0);
+  frameRate(30);
+
+  textsize = windowWidth/7;
+  textFont(font);
+  textSize(textsize);
+  textAlign(CENTER, CENTER);
+}
+
+function draw() {
+
+  clear();
+  let gap = (windowWidth*0.6)/letters.length + windowWidth*0.02;
+  let trans = map(frameCount, 0, 45, 0, windowHeight*0.35);
+  textAlign(CENTER);
+
+  for (let i=0; i<letters.length; i++){
+      let x = windowWidth*0.2 + gap*i;
+      text(char(letters[i]), x, windowHeight/2-textsize/2-trans);
+    }
+    if(frameCount==)
+  }
