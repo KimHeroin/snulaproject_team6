120d66c466b973626e889ab94ba617e4f9b558aa
KimHeroin <yebboona@snu.ac.kr>
2021-08-10 13:08:19 +0900

    c

---
+++ show.js
@@ -0,0 +1,25 @@
+function show_about(){
+  var x = document.getElementById("about");
+  if (x.style.display === "none") {
+  x.style.display = "block";
+} else {
+  x.style.display = "none";
+}
+}
+
+
+function show_team(){
+
+}
+
+function show_doc(){
+
+}
+
+function show_tut(){
+
+}
+
+function show_faq(){
+
+}
