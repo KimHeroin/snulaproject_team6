41397c3c4b49877e99dc6ee7185a3e518e287db8
KimHeroin <yebboona@snu.ac.kr>
2021-08-10 10:57:05 +0900

    c

--- introtitle.js
+++ introtitle.js
@@ -1,18 +1,39 @@
-const letters = "AGENT";
-let textsize=90;
+const title = 'AGENT';
 
-function setup() {
-  var canvas = createCanvas(windowWidth, windowHeight);
-  canvas.position(0, 0);
-  frameRate(30);
-
-  textsize = windowWidth/7;
-  textFont(font);
-  textSize(textsize);
-  textAlign(CENTER, CENTER);
+function preload(){
+  font = loadFont('AKIRA EXPANDED DEMO.OTF');
+  fontsize = windowWidth/7;
 }
 
-function draw() {
+
+function setup(){
+  var canvas= createCanvas(windowWidth, windowHeight);
+  canvas.position(0,0);
+  frameRate(30);
+
+  textFont(font);
+  textSize(fontsize);
+  textAlign(CENTER, CENTER);
+
+
+}
+
+function draw(){
   clear();
-  text(letters, (windowWidth-letters.length)/2, windowHeight/2);
-  }
+  noStroke();
+
+  let gap = (windowWidth*0.6)/title.length + windowWidth*0.02;
+
+  for (let i=0; i<title.length; i++){
+    let x = windowWidth*0.2+gap*i;
+    let t = frameCount;
+    if(frameCount>=30){t=30;}
+    let y = windowHeight*0.38*(cos(t/30*PI-PI)+1)/2;
+    fill(255*sin(t/30*PI));
+    text(title[i], x, windowHeight/2-fontsize/2-y);
+    }
+}
+
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight);
+}
