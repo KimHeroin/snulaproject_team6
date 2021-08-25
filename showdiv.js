6aadf3796f18e7c2f241095bd566d253108d82d8
KimHeroin <yebboona@snu.ac.kr>
2021-08-25 22:52:55 +0900

    c

--- showdiv.js
+++ showdiv.js
@@ -12,17 +12,3 @@ function show_cat(idx) {
   pushed_cat.style.display = "block";
 
 }
-
-function page_reset(){
-  all_page = document.querySelectorAll(".cardnews");
-  for (let i = 0; i < all_page.length; i++) {
-    all_page[i].style.display = "none";
-  }
-}
-
-function show_cardnews(idx) {
-  var pushed_cat = document.getElementsByClassName("cardnews")[idx];
-  page_reset();
-  pushed_cat.style.display = "inline-block";
-
-}
