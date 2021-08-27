0e96ee75dd2419f6f1feaaa8de203ea50116c4f0
Choseongmin <lumograph97@naver.com>
2021-08-27 16:35:47 +0900

    document

--- slideshow.js
+++ slideshow.js
@@ -24,3 +24,50 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+function cardnews_reset(){
+  all_page = document.querySelectorAll(".cardnews_folder");
+  for (let i = 0; i < all_page.length; i++) {
+    all_page[i].style.display = "none";
+  }
+}
+
+function show_cardnews0() {
+  cardnews_reset();
+  var Folder_0 = document.getElementById("folder_0");
+  Folder_0.style.display = "block";
+  
+}
+
+
+function show_cardnews1() {
+  cardnews_reset();
+  var Folder_1 = document.getElementById("folder_1");
+  Folder_1.style.display = "block";
+  
+}
+
+function show_cardnews2() {
+  cardnews_reset();
+  var Folder_2 = document.getElementById("folder_2");
+  Folder_2.style.display = "block";
+  
+}
+
+function show_cardnews3() {
+  cardnews_reset();
+  var Folder_3 = document.getElementById("folder_3");
+  Folder_3.style.display = "block";
+  
+}
+
+function show_cardnews4() {
+  cardnews_reset();
+  var Folder_4 = document.getElementById("folder_4");
+  Folder_4.style.display = "block";
+  
+}
+
+function pageUp() {
+  document.body.scrollTop = 0;
+}
